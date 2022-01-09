c12cf48985cab807979d2460cdfd1fc62fa248a7
ammar <ammarraneez@gmail.com>
2022-01-09 13:45:30 +0530

    feat: search bar

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Searchbar from './Searchbar';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="ui container">
+        <Searchbar />
+      </div>
+    )
+  }
+}
